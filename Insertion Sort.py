e12228ce60efb5679c0cdb2a4cb1784d4c71e395
ninachel <nccccc17@gmail.com>
2021-02-04 21:44:35 +0300

    -

---
+++ Insertion Sort.py
@@ -0,0 +1,14 @@
+def insertion_sort(lst):
+    n = len(lst)
+    for i in range(1, n):
+        key = lst[i]
+        j = i - 1
+        while j >= 0 and lst[j] > key:
+            lst[j + 1] = lst[j]
+            j -= 1
+        lst[j + 1] = key
+    return lst
+
+
+a = [9, 5, 4, 7, 3, 2, 1]
+print(insertion_sort(a))
