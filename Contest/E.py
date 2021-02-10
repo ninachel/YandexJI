112cde86dd59f1c5fd568832af7849f77146cd79
ninachel <nccccc17@gmail.com>
2021-02-10 12:54:16 +0300

    -

--- Contest/E.py
+++ Contest/E.py
@@ -1,15 +1,24 @@
 def anagrams(str1, str2):
     if len(str1) != len(str2):
         return 0
-    flag = 0
+    str1_letters = dict()
+    str2_letters = dict()
     for c in str1:
-        if c in str2:
-            flag = 1
+        if c in str1_letters:
+            str1_letters[c] += 1
         else:
-            return 0
-    return flag
+            str1_letters[c] = 1
+    for c in str2:
+        if c in str2_letters:
+            str2_letters[c] += 1
+        else:
+            str2_letters[c] = 1
+    return str1_letters == str2_letters
 
 
 first = input()
 second = input()
-print(anagrams(first, second))
+if anagrams(first, second):
+    print(1)
+else:
+    print(0)
