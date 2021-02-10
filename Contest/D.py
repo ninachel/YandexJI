f037abcf9ce5ad3e9fb43dae5b5f57b7f06ea0c1
ninachel <nccccc17@gmail.com>
2021-02-10 13:41:54 +0300

    -

--- Contest/D.py
+++ Contest/D.py
@@ -2,12 +2,26 @@ import itertools
 
 
 def valid_parentheses(seq):
-    pass
+    stack = []
+    for i in range(len(seq)):
+        if seq[i] == ')':
+            if not stack:
+                return False
+            else:
+                stack.pop()
+        else:
+            stack.append(seq[i])
+    if not stack:
+        return True
+    return False
 
 
 n = int(input())
 sample = "(" * n + ")" * n
-options = list(itertools.permutations(sample))
+options = list(set(itertools.permutations(sample)))
+result = []
 for x in options:
     if valid_parentheses(x):
-        print(''.join(x))
+        result.append(''.join(x))
+for x in sorted(result):
+    print(x)
